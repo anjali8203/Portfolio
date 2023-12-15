40ec1792a7063dbbde8dd742c002c89266ccb213
anjali8203 <67706483+anjali8203@users.noreply.github.com>
2023-12-14 23:23:18 -0500

    bucketlist added

--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -4,11 +4,18 @@ import { MdDone, MdClose } from "react-icons/md";
 
 function Toolstack() {
   const bucketItems = [
-    { title: "Learn to play the guitar", completed: true },
+    { title: "Make a portfolio website", completed: true },
+    { title: "Go sky diving", completed: false },
     { title: "Visit Japan", completed: false },
     { title: "Write a book", completed: false },
-    { title: "Learn a new language", completed: true },
-    // Add more items as needed...
+    { title: "Learn a new language", completed: false },
+    { title: "Run a marathon", completed: false },
+    { title: "Meet a celebrity", completed: false },
+    { title: "Do a backflip", completed: false },
+    { title: "Cook a 5 course meal", completed: false },
+    { title: "Paint or draw daily", completed: false },
+    { title: "Visit every state in the US", completed: false },
+    { title: "Give 100k to charity", completed: true },
   ];
 
   return (
