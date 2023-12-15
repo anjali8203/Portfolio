d32c907884c76f66fa631636ae897bde4f576241
anjali8203 <67706483+anjali8203@users.noreply.github.com>
2023-12-14 23:45:37 -0500

    final rah

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -6,6 +6,9 @@ import Techstack from "./Techstack";
 import Aboutcard from "./AboutCard";
 import laptopImg from "../../Assets/about.png";
 import Toolstack from "./Toolstack";
+import { AiFillGithub } from 'react-icons/ai';
+import { FaLinkedinIn } from 'react-icons/fa';
+import { MdEmail } from 'react-icons/md';
 
 function About() {
   return (
@@ -44,7 +47,46 @@ function About() {
           <strong className="purple">Bucketlist</strong>
         </h1>
         <Toolstack />
-
+         <Row>
+            <Col md={12} className="home-about-social">
+              <h1>FIND ME ON</h1>
+              <p>
+                Feel free to <span className="purple">connect </span>with me
+              </p>
+              <ul className="home-about-social-links">
+                <li className="social-icons">
+                  <a
+                    href="https://github.com/anjali8203"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="icon-colour  home-social-icons"
+                  >
+                    <AiFillGithub />
+                  </a>
+                </li>
+                <li className="social-icons">
+                  <a
+                    href="https://linkedin.com/in/anjali-mehta-a2887072"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="icon-colour  home-social-icons"
+                  >
+                    <FaLinkedinIn />
+                  </a>
+                </li>
+                <li className="social-icons">
+                  <a
+                    href="mailto:anjali8203@gmail.com"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="icon-colour  home-social-icons"
+                  >
+                    <MdEmail />
+                  </a>
+                </li>
+              </ul>
+            </Col>
+          </Row>
         {/* <Github /> */}
       </Container>
     </Container>
