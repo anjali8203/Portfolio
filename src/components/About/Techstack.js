566dbe065f4491a9ba1aa763d4c49f703127f6fc
anjali8203 <67706483+anjali8203@users.noreply.github.com>
2023-12-14 21:09:57 -0500

    Update Techstack.js

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -1,24 +1,23 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
-import { DiJava, DiPython, DiCode, DiHtml5, DiCss3, DiRProject, DiMatlab } from "react-icons/di";
 
 function Techstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
       <Col xs={4} md={2} className="tech-icons">
-        <DiCode /> {/* Placeholder for C */}
+        C
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <DiHtml5 /> {/* Placeholder for HTML */}
+        HTML
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <DiCss3 /> {/* Placeholder for CSS */}
+        CSS
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <DiJava /> {/* Java */}
+        Java
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <DiPython /> {/* Python */}
+        Python
       </Col>
     </Row>
   );
