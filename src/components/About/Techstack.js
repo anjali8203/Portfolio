e9ee65fa066eaf02a6fc199903fa4ba78c318764
anjali8203 <67706483+anjali8203@users.noreply.github.com>
2023-12-14 23:23:40 -0500

    resized tools

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -4,28 +4,28 @@ import { Col, Row } from "react-bootstrap";
 function Techstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-      <Col xs={4} md={2} className="tech-icons">
+      <Col xs={2} md={1} className="tech-icons">
         C
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
+      <Col xs={2} md={1} className="tech-icons">
         HTML
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
+      <Col xs={2} md={1} className="tech-icons">
         CSS
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
+      <Col xs={2} md={1} className="tech-icons">
         Java
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
+      <Col xs={2} md={1} className="tech-icons">
         Python
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
+      <Col xs={2} md={1} className="tech-icons">
         Matlab
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
+      <Col xs={4} md={1} className="tech-icons">
         R
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
+      <Col xs={2} md={1} className="tech-icons">
         SQL
       </Col>
     </Row>
