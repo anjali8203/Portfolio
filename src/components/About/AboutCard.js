4bd5ddbb3e397bf5958bb016fe3513fca6dc60a3
anjali8203 <67706483+anjali8203@users.noreply.github.com>
2023-12-14 23:15:47 -0500

    more personalization to about page

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -13,20 +13,29 @@ function AboutCard() {
             <br />
             I am currently a third year at University of Virginia.
             <br />
-            I'm studying Computer Science Engineering with a minor in Data Science. 
+            I'm studying Computer Science Engineering with a minor in Data Science. I have a strong interest in Machine Learning, Data Privacy, and Software Engineering. I choose to study computer science because I hope to make a positive impact on the world - and learn as much as possible while doing it! 
             <br />
             <br />
-            Here are some activities that I love: 
+            I am a hard worker, a quick learner, and very motivated! 
+            <br />
+            <br />
+            Here are some of the many hats that I wear: 
           </p>
           <ul>
             <li className="about-activity">
-              <ImPointRight /> Reading Fiction + Fantasy books 
+              <ImPointRight /> Avid reader of books (mostly fiction) 
             </li>
             <li className="about-activity">
-              <ImPointRight /> Learning to cook different foods
+              <ImPointRight /> Experimental chef 
             </li>
             <li className="about-activity">
-              <ImPointRight /> Travelling
+              <ImPointRight /> Environmental Protection Activist 
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Dog-Lover (2 dogs of my own and many fosters!)
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Traveler of the world (hopefully)
             </li>
           </ul>
 
