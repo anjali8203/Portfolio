d58a4b086bf2926c4ba8859f7e4c0ec4d64771d9
anjali8203 <67706483+anjali8203@users.noreply.github.com>
2024-06-14 16:13:19 +0200

    Update Toolstack.js

--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -5,10 +5,11 @@ import { MdDone, MdClose } from "react-icons/md";
 function Toolstack() {
   const bucketItems = [
     { title: "Make a portfolio website", completed: true },
+    { title: "Study Abroad", completed: true },
     { title: "Go sky diving", completed: false },
     { title: "Visit Japan", completed: false },
     { title: "Write a book", completed: false },
-    { title: "Learn a new language", completed: false },
+    { title: "Learn a new language", completed: true },
     { title: "Run a marathon", completed: false },
     { title: "Meet a celebrity", completed: false },
     { title: "Do a backflip", completed: false },
